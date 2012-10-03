ae948027f9e1170daa0e1edf33f174296fea7bdc
galtiepi <galtiepi@ENAC-IHM10.ihm>
2012-10-03 16:14:05 +0200

    Premier commit.

---
+++ gen/m2/ihm/smsauto9/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package m2.ihm.smsauto9;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/m2/ihm/smsauto9/NewSMS.java
@@ -0,0 +1,20 @@
+package m2.ihm.smsauto9;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class NewSMS extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_sms);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_new_sms, menu);
+        return true;
+    }
+}
